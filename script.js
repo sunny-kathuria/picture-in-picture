11a38be0fb1ffc3ecbd393a9e589061a6cb99dab
Sunny Kathuria <51099.sk@gmail.com>
2021-12-10 12:52:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement=document.getElementById('video');
+const button=document.getElementById('button');
+
+
+
+async function  selectMediaStream(){
+    try{
+        const mediaStream= await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject=mediaStream;
+        videoElement.onloadedmetadata=()=>{
+            videoElement.play();
+        }
+    } catch(error){
+        console.log("Sorry, some error here:",error);
+    }
+
+
+
+}
+
+button.addEventListener('click', async()=>{
+    button.disabled=true;
+    await videoElement.requestPictureInPicture();
+    button.disabled=false;
+})
+
+
+selectMediaStream();
